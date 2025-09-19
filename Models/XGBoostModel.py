d19bf3b508273b85d61a0e55d92c24a990ea0ed7
Jacksone2307 <Jacksone2307@gmail.com>
2025-09-19 15:13:35 +1200

    XGBoost with hyperparameter guide

--- Models/XGBoostModel.py
+++ Models/XGBoostModel.py
@@ -6,9 +6,12 @@ import matplotlib.pyplot as plt
 # from Models.SklearnModels import sklearnModel
 import xgboost as xgb
 import os
-from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold, GridSearchCV
 from sklearn.metrics import accuracy_score, f1_score, classification_report
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_classif
+
+
+
 
 # from Data.Data_Preprocessing.Data_Cleaning import extract_number
 
@@ -49,25 +52,38 @@ Y = df["outage_flag"]
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
 
 
+xgbClf = xgb.XGBClassifier(gamma = 0, max_depth = 20)
 
-xgbClf = xgb.XGBClassifier()
 
-xgbClf.fit(x_train, y_train)
+param_grid = {'max_depth': [3, 18, 25, 50, 100],
+              'learning_rate': [0.1, 0.01, 0.3, 0.5],
+              'gamma': [0, 1, 5, 10],
+            #   'booster': ['gbtree', 'gblinear', 'dart'],
+              'subsample': [0, 0.2, 0.5, 1]}
 
-y_pred = xgbClf.predict(x_test)
+grid_search = GridSearchCV(xgbClf, param_grid, cv=10, scoring='accuracy')
+grid_search.fit(x_train, y_train)
 
-print(classification_report(y_test, y_pred))
+print("Best set of hyperparameters: ", grid_search.best_params_)
+print("Best score: ", grid_search.best_score_)
 
-print(f"No CV accuracy:", accuracy_score(y_pred, y_test))
+
+# xgbClf.fit(x_train, y_train)
+
+# y_pred = xgbClf.predict(x_test)
+
+# print(classification_report(y_test, y_pred))
+
+# print(f"No CV accuracy:", accuracy_score(y_pred, y_test))
 
 #CV
-cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
-cv_acc = cross_val_score(xgbClf, X, Y, cv=cv, scoring="accuracy", n_jobs=-1).mean()
-print(f"10-fold CV accuracy:", cv_acc)
-cv_nrmse = cross_val_score(xgbClf, X, Y, cv=cv, scoring="neg_root_mean_squared_error", n_jobs=-1).mean()
-print(f"10-fold CV -RMSE:", cv_nrmse)
-cv_f1 = cross_val_score(xgbClf, X, Y, cv=cv, scoring="f1", n_jobs=-1).mean()
-print(f"10-fold CV f1:", cv_f1)
+# cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
+# cv_acc = cross_val_score(xgbClf, X, Y, cv=cv, scoring="accuracy", n_jobs=-1).mean()
+# print(f"10-fold CV accuracy:", cv_acc)
+# cv_nrmse = cross_val_score(xgbClf, X, Y, cv=cv, scoring="neg_root_mean_squared_error", n_jobs=-1).mean()
+# print(f"10-fold CV -RMSE:", cv_nrmse)
+# cv_f1 = cross_val_score(xgbClf, X, Y, cv=cv, scoring="f1", n_jobs=-1).mean()
+# print(f"10-fold CV f1:", cv_f1)
 
 
 
