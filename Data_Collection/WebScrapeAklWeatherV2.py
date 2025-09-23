76891fec022ee863495f59531a0b28f91ea6b593
timps1 <atim554@aucklanduni.ac.nz>
2025-09-23 22:37:46 +1200

    Deleting environment

--- Data_Collection/WebScrapeAklWeatherV2.py
+++ Data_Collection/WebScrapeAklWeatherV2.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 import os
 
 def webScrapADay(day,month,year, dataFilePath, dataFilename):
-    url = f"https://www.timeanddate.com/scripts/cityajax.php?n=usa/rapid-city&mode=historic&hd={year}{month}{day}&month={month}&year={year}&json=1"
+    url = f"https://www.timeanddate.com/scripts/cityajax.php?n=usa/eureka&mode=historic&hd={year}{month}{day}&month={month}&year={year}&json=1"
 
     response = requests.get(url)
     response.raise_for_status()
--- Data_Collection/run_datacollectionpipeline.sh
+++ Data_Collection/run_datacollectionpipeline.sh
@@ -3,9 +3,9 @@
 # ==============================
 # Configurable variables
 # ==============================
-wfoOfInterest="ILN"         # <-- Change this to your WFO of interest
+wfoOfInterest="EKA"         # <-- Change this to your WFO of interest
 path_to="../../Weather_Data/"               # <-- Path containing {hfo}_wwa.csv
-weather_data_folder="../../Weather_Data/ILN/"      # Folder where all weather data CSVs are saved
+weather_data_folder="../../Weather_Data/EKA/"      # Folder where all weather data CSVs are saved
 
 # ==============================
 # Step 3: Clean the data
