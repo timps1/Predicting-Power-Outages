Fixed normalisation

Data_Collection/NormaliseData.py
@@ -5,7 +5,7 @@ import sys
 def main():
     df = pd.read_csv(sys.argv[1])
     methodLabel='zero_to_one'
-    normalizeDataByColumn(df,  methodLabel)
+    df = normalizeDataByColumn(df,  methodLabel)
     dotIndex = sys.argv[1].rfind(".")
     nexFilename = sys.argv[1][:dotIndex] + "_normalised" + ".csv"
     df.to_csv(nexFilename, index=False)
@@ -14,7 +14,7 @@ def normalizeDataByColumn(data, methodLabel='zero_to_one'):
     """
     Normalize each column of data.
     """
-    print("Processed columns:", end=" | ")
+    print("Processed columns:")
     for columnName in data.columns:
         print(columnName, end=" | ")
         if 'outage' in columnName:  # Skip target column
@@ -31,9 +31,9 @@ def normalizeDataByColumn(data, methodLabel='zero_to_one'):
         elif "Rain" in columnName:
             data[columnName] = data[columnName] / 5
         elif methodLabel == 'zero_to_one':
-            normalizeDataZeroToOne(data, columnName)
+            data = normalizeDataZeroToOne(data, columnName)
         elif methodLabel == 'minus_one_to_one':
-            normalizeDataMinusOneToOne(data, columnName)
+            data = normalizeDataMinusOneToOne(data, columnName)
         
         ######## Add more normalization methods with methodLabel as needed #########
         
@@ -41,19 +41,23 @@ def normalizeDataByColumn(data, methodLabel='zero_to_one'):
             raise ValueError(f"Normalization method '{methodLabel}' is not supported." + 
                                 "\nMake sure to add the method label normalizeData" +
                                 " to the Data_Augmentation/NormaliseData.py file.")
-        
+        print("Normalised")
     print()
+    return data
 def normalizeDataZeroToOne(data, columnName):
     min_val = np.min(data[columnName])
     max_val = np.max(data[columnName])
     if min_val != max_val:
         data[columnName] = (data[columnName] - min_val) / (max_val - min_val)
+    return data
     
 
 def normalizeDataMinusOneToOne(data, columnName):
     min_val = np.min(data[columnName], axis=0)
     max_val = np.max(data[columnName], axis=0)
-    data[columnName] = 2 * (data[columnName] - min_val) / (max_val - min_val) - 1
+    if min_val != max_val:
+        data[columnName] = 2 * (data[columnName] - min_val) / (max_val - min_val) - 1
+    return data
 
 ###########################################################################
 # Additional methods can be added here for other normalization techniques #